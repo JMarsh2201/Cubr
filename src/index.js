5fe56be4ad37752a13c8d91c95408d64d34b97ab
normanthewhale <wmhale92@gmail.com>
2019-10-24 15:03:48 -0600

    Added ReactJS to the front end.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-const mtg = require('mtgsdk')
-import './style.css'
+import './index.css'
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#App');
+ReactDOM.render(e(LikeButton), domContainer);
